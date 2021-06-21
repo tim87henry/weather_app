16ddffada110b18706effdc40951161314cc3caf
Timothy <tim87henry@gmail.com>
2021-06-21 22:47:50 +1000

    Used mixed content work around, to fix github page issue

--- src/performAPICall.js
+++ src/performAPICall.js
@@ -1,5 +1,5 @@
 async function performAPICall(locn) {
-    let p = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+locn+'&units=metric&appid=2d62a90b58e587895e43e52c309887b6',{mode: "cors"})
+    let p = await fetch('https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q='+locn+'&units=metric&appid=2d62a90b58e587895e43e52c309887b6',{mode: "cors"})
     return await p.json()
 }
 
